5ed1f54a26f95c3797daf5faa9a1478c47c85ec8
DARK CRIME <86085229+DARKCRIME1@users.noreply.github.com>
2022-07-19 01:00:16 +0530

    Update main.js

--- main/main.js
+++ main/main.js
@@ -3,11 +3,11 @@ __path = process.cwd()
 var express = require('express');
 var router = express.Router();
 
-router.get('/film', (req, res) => {
-    res.sendFile(__path + '/page/index1.html')
+router.get('/home', (req, res) => {
+    res.sendFile(__path + '/page/index.html')
 })
-router.get('/404', (req, res) => {
-    res.sendFile(__path + '/page/404.html')
+router.get('/english', (req, res) => {
+    res.sendFile(__path + '/page/english.html')
 })
 router.get('/', (req, res) => {
     res.sendFile(__path + '/page/index.html')
